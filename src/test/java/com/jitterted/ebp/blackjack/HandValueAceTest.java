27515aef21d7c9d0e5882207bda320aa18f8e812
Ted M. Young <tedyoung@gmail.com>
2020-08-26 12:08:31 -0700

    Added Ace tests that uncovered a bug and we fixed it by changing < 11 to <= 11.

--- src/test/java/com/jitterted/ebp/blackjack/HandValueAceTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/HandValueAceTest.java
@@ -6,6 +6,25 @@ import static org.assertj.core.api.Assertions.*;
 
 public class HandValueAceTest {
 
+  // 1: A + 10: A = 11
+  // 2: A + 12: A = 1
+
+  @Test
+  public void handWithOneAceAndTwoCardsValuedAt10ShouldTotal21() throws Exception {
+    Hand hand = HandCreator.createHandOf("A", "5", "5");
+
+    assertThat(hand.value())
+        .isEqualTo(11 + 5 + 5);
+  }
+
+  @Test
+  public void handWithOneAceAndOtherCardsValuedAt12ShouldTotal13() throws Exception {
+    Hand hand = HandCreator.createHandOf("A", "6", "6");
+
+    assertThat(hand.value())
+        .isEqualTo(1 + 6 + 6);
+  }
+
   @Test
   public void handWithOneAceTwoCardsIsValuedAt11() throws Exception {
     Hand hand = HandCreator.createHandOf("A", "5");
